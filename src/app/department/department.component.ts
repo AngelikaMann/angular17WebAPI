8a9436bb42ac1d5ce236fc03a115923e50b001a9
AngelikaMann <angelika0769@gmail.com>
2023-12-01 13:51:11 +0100

    Department mit SQL Server verbunden

--- src/app/department/department.component.ts
+++ src/app/department/department.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ShowDepComponent } from "./show-dep/show-dep.component";
+import { AddEditDepComponent } from './add-edit-dep/add-edit-dep.component';
 @Component({
   selector: 'app-department',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ShowDepComponent, AddEditDepComponent],
   templateUrl: './department.component.html',
   styleUrl: './department.component.css'
 })
--- src/app/department/show-dep/show-dep.component.ts
+++ src/app/department/show-dep/show-dep.component.ts
@@ -1,12 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedService } from '../../shared.service';
+import { AddEditDepComponent } from '../add-edit-dep/add-edit-dep.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-show-dep',
   standalone: true,
-  imports: [],
   templateUrl: './show-dep.component.html',
-  styleUrl: './show-dep.component.css'
+  styleUrl: './show-dep.component.css',
+  imports: [CommonModule, AddEditDepComponent, FormsModule],
 })
-export class ShowDepComponent {
+export class ShowDepComponent implements OnInit {
+  constructor(private service: SharedService) {}
+  DepartmentList: any = [];
+  dataItem: any;
+  ModalTitle!: string;
+  ActivateAddEditDepComp: boolean = false;
+  dep: any;
+
+  DepartmentIdFilter:string="";
+  DepartmentNameFilter:string="";
+  DepartmentListWithoutFilter:any=[];
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  addClick() {
+    this.dep = {
+      DepartmentId: 0,
+      DepartmentName: '',
+    };
+    this.ModalTitle = 'Add Department';
+    this.ActivateAddEditDepComp = true;
+  }
+
+  closeClick() {
+    this.ActivateAddEditDepComp = false;
+    this.refreshDepList();
+  }
+  editClick(item: any) {
+    this.dep = item;
+    this.ModalTitle = 'Edit Department';
+    this.ActivateAddEditDepComp = true;
+  }
+  deleteClick(item: any) {
+    if (confirm('Are you sure?')) {
+      this.service.deleteDepartment(item.DepartmentId).subscribe((data) => {
+        alert(data.toString());
+        this.refreshDepList();
+      });
+    }
+
+    this.dep = item;
+    this.ModalTitle = 'Edit Department';
+    this.ActivateAddEditDepComp = true;
+  }
+
+  refreshDepList() {
+    this.service.getDepList().subscribe((data) => {
+      this.DepartmentList = data;
+      this.DepartmentListWithoutFilter=data;
+    });
+  }
+
+
+
 
 }
