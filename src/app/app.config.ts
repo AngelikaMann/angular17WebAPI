a076c74bd32106ff1bc8a7777c853795794f0562
AngelikaMann <angelika0769@gmail.com>
2023-12-01 12:59:27 +0100

    Routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter,Routes } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
+import { provideHttpClient } from '@angular/common/http';
+
+export const appRoutes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+];
+
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [provideRouter(routes), provideHttpClient(), provideClientHydration()]
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { EmployeeComponent } from './employee/employee.component';
+import { DepartmentComponent } from './department/department.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'employee', component: EmployeeComponent },
+  { path: 'department', component: DepartmentComponent },
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
+
+
