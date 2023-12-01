badd1f9f609a177281c9bf21f279860e06fc52a9
AngelikaMann <angelika0769@gmail.com>
2023-12-01 14:33:45 +0100

    EmployeesListe mit SQL Server verbunden

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ShowEmpComponent } from "./show-emp/show-emp.component";
+import { AddEditEmpComponent } from './add-edit-emp/add-edit-emp.component';
 @Component({
   selector: 'app-employee',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,ShowEmpComponent,AddEditEmpComponent],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
--- src/app/employee/show-emp/show-emp.component.ts
+++ src/app/employee/show-emp/show-emp.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedService } from '../../shared.service';
+import { AddEditEmpComponent } from '../add-edit-emp/add-edit-emp.component';
 
 @Component({
   selector: 'app-show-emp',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, AddEditEmpComponent],
   templateUrl: './show-emp.component.html',
-  styleUrl: './show-emp.component.css'
+  styleUrl: './show-emp.component.css',
 })
-export class ShowEmpComponent {
+export class ShowEmpComponent implements OnInit {
 
+  constructor(private service: SharedService) {}
+  EmployeeList: any = [];
+  dataItem: any;
+  ModalTitle!: string;
+  ActivateAddEditEmpComp: boolean = false;
+  emp: any;
+
+  ngOnInit(): void {
+    this.refreshEmpList();
+  }
+
+  addClick() {
+    this.emp = {
+      EmployeeId: 0,
+      EmployeeName: '',
+      Department:"",
+       DateOfJoining:"",
+       PhotoFileName:"anonymous.png"
+    };
+    this.ModalTitle = 'Add Employee';
+    this.ActivateAddEditEmpComp = true;
+  }
+
+  closeClick() {
+    this.ActivateAddEditEmpComp = false;
+    this.refreshEmpList();
+  }
+  editClick(item: any) {
+    console.log(item)
+    this.emp = item;
+    this.ModalTitle = 'Edit Employee';
+    this.ActivateAddEditEmpComp = true;
+  }
+  deleteClick(item: any) {
+    if (confirm('Are you sure?')) {
+      this.service.deleteEmployee(item.EmployeeId).subscribe((data) => {
+        alert(data.toString());
+        this.refreshEmpList();
+      });
+    }
+
+    this.emp = item;
+    this.ModalTitle = 'Edit Employee';
+    this.ActivateAddEditEmpComp = true;
+  }
+
+  refreshEmpList() {
+    this.service.getDepEmplList().subscribe((data) => {
+      this.EmployeeList = data;
+    });
+  }
 }
